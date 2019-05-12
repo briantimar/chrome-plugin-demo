d2f5f86cc7c0ca22ccaf8e95a0afffaa055a578e
brian <timarbrian@gmail.com>
2019-05-12 16:55:24 -0700

    added declarative content boilerplate

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
 chrome.runtime.onInstalled.addListener( function() {
+        // store a shade of green under the name 'color'
         chrome.storage.sync.set({color: '#3aa757'}, function() {
             console.log("Color set to green");
+        });
+        // shows the page action (ie button in the upper right toolbar) whenever the host is developer.chrome.com
+        chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+            chrome.declarativeContent.onPageChanged.addRules([{
+                conditions: [new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'developer.chrome.com'},
+                            })
+                        ],
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }]);
         });
 });
